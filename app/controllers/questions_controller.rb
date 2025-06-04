17aea168b04ffe6945eb66c8792686c6a80775ac
Audric Nomentsoa <audricmarshall@gmail.com>
2025-06-04 21:20:53 +0200

    ajoute la logique du controller pour afficher la reponse du coach selon la question posee

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,19 @@
 class QuestionsController < ApplicationController
   def ask
+    # la view est déjà affichée
   end
 
   def answer
+    user_question = params[:question] # recupere la question envoyée depuis le form
+    puts "question envoyee par user: #{user_question}" # debug pour afficher la question reçue dans rails
+
+    # logique de reponse du coach
+    if user_question == 'I am going to work'
+      @answer = 'Great!'
+    elsif user_question&.end_with?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = 'I don\'t care, get dressed and go to work!'
+    end
   end
 end
